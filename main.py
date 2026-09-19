0a00f826d293cb564cae3123083b55d1044c9519
Ankit Khanal <ankitkhanal231@gmail.com>
2026-09-19 17:14:17 +0545

    feat: use enum to create specific valued route params

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -22,3 +24,20 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user_detail(user_id: int):
     return {"user_id": user_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
