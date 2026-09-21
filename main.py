247e062483f618a741ae8e2d4e1c6326823ca828
Ankit Khanal <ankitkhanal231@gmail.com>
2026-09-21 22:13:20 +0545

    chore: add next

--- main.py
+++ main.py
@@ -61,3 +61,6 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_items(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+
+# Next: https://fastapi.tiangolo.com/tutorial/body/
